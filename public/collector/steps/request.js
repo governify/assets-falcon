ac65a9d1d24348bea86bb8ad459f178c8f5f36d5
alesancor1 <alesancor1@alum.us.es>
2021-11-14 20:55:03 +0100

    modified request for calculation

--- public/collector/steps/request.js
+++ public/collector/steps/request.js
@@ -2,35 +2,38 @@
 /* eslint-disable no-async-promise-executor */
 
 const governify = require('governify-commons');
-const config = require('./config')
-const credentials = config.credentials();
 const _ = require('lodash')
 
 async function applyStep (dsl, period, inputs, responseList) {
   return new Promise(async function (resolve, reject) {
     const url = dsl.config.url + inputs.request.endpoint;
 
-    const body = JSON.parse(JSON.stringify(inputs.request.body).replace(/>>>period.from<<</g, period.from).replace(/>>>period.to<<</g, period.to));
+    const params = JSON.parse(JSON.stringify(inputs.request.params).replace(/>>>period.from<<</g, period.from).replace(/>>>period.to<<</g, period.to));
     const res = await governify.httpClient.request({
       url: url,
-      method: 'POST',
-      headers: { Authorization: credentials.elk},
-      data: body
+      params: params,
+      method: inputs.request.method,
+      body: inputs.request.body
     }).then(response => { return response.data; }).catch(console.log);
 
-    var resultList = _.get(res, 'aggregations.services.buckets');
+    var resultList = _.get(res, inputs.result.valueAddress);
     var finalResult = [];
     resultList.forEach(rs => {
       var newScope = Object.assign({}, dsl.metric.scope);
-      newScope.servicio = rs.key;
-      var metricResult = rs.avgresponsetime ? rs.avgresponsetime.value : rs.doc_count;
-      finalResult.push({ evidences: res.hits.hits, metric: metricResult != null ? metricResult : 0, scope: newScope });
+      newScope.service = rs.metric[inputs.result.scopeKey];
+      var metricResult = _avg(rs.values.map(vals => vals[1]).filter(val => val != "NaN"));
+      finalResult.push({ evidences : rs.values, metric: metricResult, scope: newScope });
     });
-
     // Add the result to the current result from previous steps.
     var resultConcat = responseList.concat(finalResult);
     resolve(resultConcat);
   });
+}
+
+function _avg(arr){
+  if (arr.length === 0) return 0;
+  if (arr.length === 1) return parseFloat(arr[0]);
+  return arr.reduce((a, b) => a + b, 0) / arr.length;
 }
 
 module.exports.applyStep = applyStep;
